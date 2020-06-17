4f0485d296c6f1a4555e7620720793d1aa9e7699
Tijmen Smit <code@tijmensmit.nl>
2020-06-17 13:46:17 +0200

    Created companion device pairing class

--- app/src/main/java/nl/tijmensmit/breathlinebluetooth/MainActivity.java
+++ app/src/main/java/nl/tijmensmit/breathlinebluetooth/MainActivity.java
@@ -2,7 +2,15 @@ package nl.tijmensmit.breathlinebluetooth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.companion.AssociationRequest;
+import android.companion.BluetoothLeDeviceFilter;
+import android.companion.CompanionDeviceManager;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
+
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +19,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    MyDeviceSelectionActivity connect = new MyDeviceSelectionActivity();
+
 }
+
