f786651dea058514f6bb9408a89bf0d18354e454
Tijmen Smit <code@tijmensmit.nl>
2020-06-21 10:54:14 +0200

    Created separate Bluetooth class

---
+++ app/src/main/java/nl/tijmensmit/breathlinebluetooth/Bluetooth.java
@@ -0,0 +1,92 @@
+package nl.tijmensmit.breathlinebluetooth;
+
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.util.Log;
+
+public final class Bluetooth {
+    private final static String TAG = Bluetooth.class.getSimpleName();
+
+    private BLEService mBluetoothLeService;
+    private String mDeviceAddress;
+    private boolean mConnected = false;
+    private Context context;
+
+    private final ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            mBluetoothLeService = ((BLEService.LocalBinder) service).getService();
+            if (!mBluetoothLeService.initialize()) {
+                Log.e(TAG, "Unable to Initialize");
+//                finish();
+            }
+            mBluetoothLeService.connect(mDeviceAddress);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mBluetoothLeService = null;
+        }
+    };
+
+    private final BroadcastReceiver mGattUpdateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            if (BLEService.ACTION_GATT_CONNECTED.equals(action)) {
+                mConnected = true;
+                // Other actions?
+            } else if (BLEService.ACTION_GATT_DISCONNECTED.equals(action)){
+                mConnected = false;
+                // ...
+            } else if (BLEService.ACTION_GATT_SERVICES_DISCOVERED.equals(action)){
+                Log.d(TAG, "Supported Services:" + mBluetoothLeService.getSupportedGattServices());
+                //Recieve gattService as List<BluetoothGattService>
+            } else if (BLEService.ACTION_DATA_AVAILABLE.equals(action)){
+                Log.d(TAG, "Extra Data Available:" + BLEService.EXTRA_DATA);
+                // Recieve data as String
+            }
+        }
+    };
+
+    public Bluetooth(Context context){
+        this.context = context;
+        Intent gattServiceIntent = new Intent(context, BLEService.class);
+        context.bindService(gattServiceIntent, mServiceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+
+    public void connect() {
+        mDeviceAddress = "80:1F:12:BE:2B:7C";
+        context.registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
+        if (mBluetoothLeService != null) {
+            final boolean result = mBluetoothLeService.connect(mDeviceAddress);
+            Log.d(TAG, "Connect request result:" + result);
+        }
+    }
+
+    private static IntentFilter makeGattUpdateIntentFilter() {
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(BLEService.ACTION_GATT_CONNECTED);
+        intentFilter.addAction(BLEService.ACTION_GATT_DISCONNECTED);
+        intentFilter.addAction(BLEService.ACTION_GATT_SERVICES_DISCOVERED);
+        intentFilter.addAction(BLEService.ACTION_DATA_AVAILABLE);
+        return intentFilter;
+    }
+
+
+    public void pause() {
+        context.unregisterReceiver(mGattUpdateReceiver);
+    }
+
+
+    public void stop() {
+        context.unbindService(mServiceConnection);
+        mBluetoothLeService = null;
+    }
+}
--- app/src/main/java/nl/tijmensmit/breathlinebluetooth/MainActivity.java
+++ app/src/main/java/nl/tijmensmit/breathlinebluetooth/MainActivity.java
@@ -23,73 +23,32 @@ import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = MainActivity.class.getSimpleName();
-    private BLEService mBluetoothLeService;
-    private String mDeviceAddress;
-    private boolean mConnected = false;
-
-    private final ServiceConnection mServiceConnection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName componentName, IBinder service) {
-            mBluetoothLeService = ((BLEService.LocalBinder) service).getService();
-            if (!mBluetoothLeService.initialize()) {
-                Log.e(TAG, "Unable to Initialize");
-                finish();
-            }
-            mBluetoothLeService.connect(mDeviceAddress);
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName componentName) {
-            mBluetoothLeService = null;
-        }
-    };
-
-    private final BroadcastReceiver mGattUpdateReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            final String action = intent.getAction();
-            if (BLEService.ACTION_GATT_CONNECTED.equals(action)) {
-                mConnected = true;
-                // Other actions?
-            } else if (BLEService.ACTION_GATT_DISCONNECTED.equals(action)){
-                mConnected = false;
-                // ...
-            } else if (BLEService.ACTION_GATT_SERVICES_DISCOVERED.equals(action)){
-                Log.i(TAG, "Supported Services:" + mBluetoothLeService.getSupportedGattServices());
-            } else if (BLEService.ACTION_DATA_AVAILABLE.equals(action)){
-                Log.i(TAG, "Extra Data Available:" + BLEService.EXTRA_DATA);
-            }
-        }
-    };
+    private Bluetooth mBluetooth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mDeviceAddress = "80:1F:12:BE:2B:7C";
-
-        Intent gattServiceIntent = new Intent(this, BLEService.class);
-        bindService(gattServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
+        mBluetooth = new Bluetooth(this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
-        if (mBluetoothLeService != null) {
-            final boolean result = mBluetoothLeService.connect(mDeviceAddress);
-            Log.d(TAG, "Connect request result=" + result);
-        }
+        mBluetooth.connect();
     }
-    private static IntentFilter makeGattUpdateIntentFilter() {
-        final IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction(BLEService.ACTION_GATT_CONNECTED);
-        intentFilter.addAction(BLEService.ACTION_GATT_DISCONNECTED);
-        intentFilter.addAction(BLEService.ACTION_GATT_SERVICES_DISCOVERED);
-        intentFilter.addAction(BLEService.ACTION_DATA_AVAILABLE);
-        return intentFilter;
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mBluetooth.pause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mBluetooth.stop();
     }
 
 }
